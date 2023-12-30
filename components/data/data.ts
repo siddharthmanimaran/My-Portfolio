fd4aa5781c93e7fbc199ace5a56d49580ae31136
siddharth2898 <siddharth.tt@drivex.in>
2023-12-30 16:20:14 +0530

    about updated

--- components/data/data.ts
+++ components/data/data.ts
@@ -1,17 +1,22 @@
 import { RiComputerLine } from "react-icons/ri";
+import { FaReact } from "react-icons/fa";
+import { IoLogoNodejs } from "react-icons/io5";
+import { SiMongodb } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { SiCss3 } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiTypescript } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
 
 export const aboutData = [
-  {
-    id: 1,
-    title: "Frontend",
-    body: "React",
-    Icon: RiComputerLine,
-  },
-  {
-    id: 2,
-    title: "Backend",
-    body: "NodeJS",
-    Icon: RiComputerLine,
-  },
-  { id: 3, title: "DataBase", body: "MongoDB", Icon: RiComputerLine },
+  { id: 1, title: "Frontend", body: "HTML", Icon: FaHtml5 },
+  { id: 1, title: "Frontend", body: "CSS", Icon: SiCss3 },
+  { id: 1, title: "Frontend", body: "JavaScript", Icon: IoLogoJavascript },
+  { id: 1, title: "Frontend", body: "TypeScript", Icon: SiTypescript },
+  { id: 1, title: "Frontend", body: "React", Icon: FaReact },
+  { id: 1, title: "Frontend", body: "NextJs", Icon: SiNextdotjs },
+  { id: 2, title: "Backend", body: "NodeJS", Icon: IoLogoNodejs },
+  { id: 2, title: "Backend", body: "Express", Icon: SiExpress },
+  { id: 3, title: "DataBase", body: "MongoDB", Icon: SiMongodb },
 ];
